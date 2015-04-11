0fc9bb26b2c02db884ad7a3d1f7070e2f7d358b7
hognijons <hoggih5@gmail.com>
2015-04-11 15:30:04 +0000

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var app = express();
-app.use('/', express.static(__dirname + '/public'));
+app.use('/', express.static(__dirname + '/site'));
 var port = Number(process.env.PORT || 5000);
 app.listen(port, function() { 
     console.log('Your files will be served through this web server')
